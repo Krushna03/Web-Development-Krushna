24fab828060d6dcc680dbfc3b85ef61a926b6683
krushna <krushnasakhare965@gmail.com>
2024-01-19 20:35:28 +0530

    Added Calculator & RockPaperScissor Game Folder

---
+++ JavaScript - SuperDev/Js Code/Calculator/calculate.js
@@ -0,0 +1,15 @@
+    let calculation = localStorage.getItem('Calculation') || '';
+
+     function updateCalculation(value){
+         calculation = calculation + value;
+
+         Output();
+
+         localStorage.setItem('Calculation' , calculation);
+     }
+    
+    
+    function Output() {
+     document.querySelector('.result')
+      .innerHTML = calculation;
+   }  
